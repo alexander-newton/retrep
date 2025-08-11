1f636d92a6f78cd6109a2cfd3654ac0817e74169
shah-meet0 <meetsamshah@gmail.com>
2025-08-11 12:32:49 +0100

    updated replication.py

--- replication.py
+++ replication.py
@@ -1,17 +1,11 @@
+import numpy as np
 from loglinearcorrection.correction_estimator import DoublyRobustElasticityEstimator
 import os
 from datetime import datetime
+import json
+import pandas as pd
 # VERY MUCH WORK IN PROGRESS
 
-"""
-Metadata format:
-{
-'PaperID' : Numeric,
-'Table': Numeric,
-'Result Number': Numeric,
-'Binary_x': Boolean
-}
-"""
 class Replicator:
     def __init__(self, metadata,  y, X, interest, fe = None, endog_x = None, z = None, elasticity=False, replicated=False, kwargs_estimator=None, kwargs_fit=None, kwargs_ols = None, kwargs_ppml=None, cluster=None, comments=None):
         self.y = y # Outcome of interest
@@ -19,8 +13,8 @@ class Replicator:
         self.instruments = z
         self.interest = interest # Name of the exogenous variable of interest
         self.cluster = cluster
-        self.kwargs_ols = kwargs_ols if kwargs_ols is not None else {'cov_type':'HC0_se'}
-        self.kwargs_ppml = kwargs_ppml if kwargs_ppml is not None else {'cov_type':'HC0_se'}
+        self.kwargs_ols = kwargs_ols if kwargs_ols is not None else {'cov_type':'HC0'}
+        self.kwargs_ppml = kwargs_ppml if kwargs_ppml is not None else {'cov_type':'HC0'}
         self.comments = comments if comments is not None else 'No comments provided'
 
         if cluster is not None:
@@ -45,11 +39,11 @@ class Replicator:
         )
 # Make sure x and z are together
     def replicate_ols(self, weights=None):
-        results_ols = self.estimator._base_ols_fit(weights=weights, **self.kwargs_ols)
+        results_ols = self.estimator._fit_base_ols(weights=weights, **self.kwargs_ols)
         return results_ols
 
     def replicate_ppml(self, weights=None, **kwargs):
-        results_ppml = self.estimator._base_ppml_fit(weights=weights, **self.kwargs_ppml)
+        results_ppml = self.estimator._fit_base_ppml(weights=weights, **self.kwargs_ppml)
         return results_ppml
 
     def save_output(self, data_dir, output_dir):
@@ -65,57 +59,75 @@ class Replicator:
         metadata_dict['panel_identifier'] = self.metadata['panel_identifier']
         metadata_dict['model_type'] = self.metadata['model_type']
         metadata_dict['elasticity'] = self.elasticity
-        metadata_dict['interest'] = self.interest
+        if isinstance(self.interest, np.ndarray):
+            metadata_dict['interest'] = list(self.interest)
+        elif isinstance(self.interest, list):
+            metadata_dict['interest'] = self.interest
+        else:
+            metadata_dict['interest'] = [self.interest]
         metadata_dict['comments'] = self.comments
 
         if self.estimator.fe is not None:
-            metadata_dict['fe'] = self.estimator.fe_indices
+            metadata_dict['fe'] = list(self.estimator.fe_indices)
 
         if self.estimator.instruments is not None:
-            metadata_dict['endogenous_regressors'] = self.estimator.endog_x
+            metadata_dict['endogenous_regressors'] = list(self.estimator.endog_x)
 
         metadata_dict['kwargs_ols'] = self.kwargs_ols
         metadata_dict['kwargs_ppml'] = self.kwargs_ppml
         self.metadata['time'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         if self.cluster is not None:
-            metadata_dict['cluster'] = self.cluster
+            metadata_dict['cluster'] = list(self.cluster)
 
         # fnc to save metadata based on paper_id
+        metadata_folder = os.path.join(output_dir, str(paper_id))
+        if not os.path.exists(metadata_folder):
+            os.makedirs(metadata_folder)
+
+        metadata_file = os.path.join(metadata_folder, f'metadata_{metadata_dict['table_id']}_{metadata_dict['panel_identifier']}.json')
+        with open(metadata_file, 'w') as f:
+            json.dump(metadata_dict, f, default=str)
+
+
 
         # fnc to save data based on paper_id
-        data_y = self.y
-        data_X = self.X
-        data_instruments = self.estimator.instruments if self.estimator.instruments is not None else None
+        data_folder = os.path.join(data_dir, str(paper_id))
+        if not os.path.exists(data_folder):
+            os.makedirs(data_folder)
+
+
+        data_y = pd.DataFrame(self.y)
+        data_X = pd.DataFrame(self.X)
+        data_instruments = pd.DataFrame(self.estimator.instruments) if self.estimator.instruments is not None else None
+
 
         # save data as parquet
+        data_y.to_parquet(data_folder + f'/{metadata_dict['panel_identifier']}_y')
+        data_X.to_parquet(data_folder + f'/{metadata_dict['panel_identifier']}_X')
+
+        if data_instruments is not None:
+            data_instruments.to_parquet(data_folder + f'/{metadata_dict['panel_identifier']}_Z')
 
 
 
-
-
-
-
-
-
-
-
-
-def replicate(metadata, y, X, interest, fe=None, z=None, elasticity=False, replicated=False, kwargs_estimator=None, kwargs_fit=None, kwargs_ols=None, kwargs_ppml=None, fit_full_model=False, output=False, data_dir= None, output_dir=None):
+def replicate(metadata, y, X, interest, endog_x=None, z=None, fe=None, elasticity=False, replicated=False, kwargs_estimator=None,
+              kwargs_fit=None, kwargs_ols=None, kwargs_ppml=None, fit_full_model=False, output=False,
+              data_dir= None, output_dir=None):
     """
     Replicate the results using the provided metadata and data.
     """
     # Add covtype hc3 here maybe?
 
-    replicator = Replicator(metadata, y, X, interest, fe=fe, z=z, elasticity=elasticity, replicated=replicated, kwargs_estimator=kwargs_estimator, kwargs_fit=kwargs_fit, kwargs_ols=kwargs_ols, kwargs_ppml=kwargs_ppml)
-    ols_results = replicator.replicate_ols(weights=None, **kwargs_ols)
+    replicator = Replicator(metadata, y, X, interest, endog_x=endog_x, z=z, fe=fe, elasticity=elasticity, replicated=replicated, kwargs_estimator=kwargs_estimator, kwargs_fit=kwargs_fit, kwargs_ols=kwargs_ols, kwargs_ppml=kwargs_ppml)
+    ols_results = replicator.replicate_ols(weights=None)
     print('OLS res:')
-    print(ols_results.summary())
-    ppml_results = replicator.replicate_ppml(weights=None, **kwargs_ppml)
-    print('PPML res:')
-    print(ppml_results.summary())
-    if fe is not None:
-        print('PPML results currently inaccurate with fixed effects')
+    print(ols_results.summary(yname=replicator.estimator.endog_names,xname=list(replicator.estimator.exog_names)))
+    # ppml_results = replicator.replicate_ppml(weights=None)
+    # print('PPML res:')
+    # print(ppml_results.summary())
+    # if fe is not None:
+    #     print('PPML results currently inaccurate with fixed effects')
 
     if fit_full_model:
         print('Full model fitting will be added soon, patience!')
