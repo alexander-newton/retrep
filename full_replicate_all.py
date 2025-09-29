85ab8fa4d7ca38e1b129721bfcb9c4310f57e18a
shah-meet0 <meetsamshah@gmail.com>
2025-09-29 10:03:28 +0100

    added standard errors to full replicate

--- full_replicate_all.py
+++ full_replicate_all.py
@@ -32,9 +32,9 @@ def run_replications(DIR):
 
             # TODO: fix how kwargs are stored and loaded. Right now they are not stored appropriately.
             print(f"Processing paper {stripped_path[-2]}, {stripped_path[-1]}")
-            if stripped_path[-2] in ['018', '050']:
-                print("Skipping paper for now due to data issues.")
-                continue
+            # if stripped_path[-2] in ['018', '050']:
+            #     print("Skipping paper for now due to data issues.")
+            #     continue
             try:
                 metadata = os.path.join(result_path, 'metadata.json')
                 X = pd.read_parquet(os.path.join(result_path, 'X.parquet'))
@@ -63,8 +63,8 @@ def run_replications(DIR):
                 print(f"Error loading data for {result_path}: {e}")
                 continue
 
-    all_results_df = pd.DataFrame(all_results)
-    all_results_df.to_csv('./trial_results.csv')
+        all_results_df = pd.DataFrame(all_results)
+        all_results_df.to_csv('./trial_results_se.csv')
 
 
 
@@ -80,22 +80,24 @@ def replicate_full(X,y, z, metadata):
     kwargs_ols = metadata.get('kwargs_ols', {})
     endog_x = metadata.get('endogenous_regressors', None)
 
-    nnparams = {
-        'num_layers': 8,
-        'num_units': 128
+    # nnparams = {
+    #     'num_layers': 8,
+    #     'num_units': 128
+    #
+    # }
 
-    }
-
-    estimator = DoublyRobustElasticityEstimator(endog=y, exog=X, interest=interest, endog_x=endog_x, instruments=z, fe=fe, estimator_type='nn', nn_params=nnparams, elasticity=elasticity)
-    fit = estimator.fit(method='ols', compute_asymptotic_variance=False, weights=weights)
+    estimator = DoublyRobustElasticityEstimator(endog=y, exog=X, interest=interest, endog_x=endog_x, instruments=z, fe=fe, estimator_type='nn', elasticity=elasticity)
+    fit = estimator.fit(method='ols', bootstrap=True, bootstrap_reps=100, weights=weights)
     print(fit.summary())
-    res_ppml = fit_ppml_or_ivppml(y, X, instruments=z, fe_cols=fe, endog_cols=endog_x, interest_var=interest, hdfe=fe is not None, return_meta=True)
+    # res_ppml = fit_ppml_or_ivppml(y, X, instruments=z, fe_cols=fe, endog_cols=endog_x, interest_var=interest, hdfe=fe is not None, return_meta=True)
     res_dict = {
         'beta': fit.parametric_coef[fit.interest[0]],
+        'beta_se': fit.parametric_results.bse[fit.interest[0]],
         'average_estimate': fit.average_estimate(),
         'estimate_at_average': fit.estimate_at_average(),
+        'bootstrap_se': fit.bootstrap_se_dict[fit.interest[0]][3],
         'correction': np.mean(fit.correction[fit.interest[0]]),
-        'ppml_beta': res_ppml[2]['beta']
+        # 'ppml_beta': res_ppml[2]['beta']
     }
     print(res_dict)
     return res_dict
